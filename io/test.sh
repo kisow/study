fdd77b7b1c3546bddb1e7a1976bf6cc8078472f5
김재훈 <kisow@navercorp.com>
2014-07-17 11:21:08 +0900

    io 테스트 수정

--- io/test.sh
+++ io/test.sh
@@ -10,7 +10,8 @@ run()
 {
 	for i in 512 1024 2048 3584 4096 8192 16384 32768 65536; 
 	do
-		time envoke ./o_direct_test data $i
+		dd if=/dev/zero of=.data count=1 bs=$i
+		time envoke ./o_direct_test .data $i
 	done
 }
 
